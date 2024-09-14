284536ddc93a0df6751586363822ef481f9a7626
rmocq <romuald.mocq@plume-solution.com>
2024-09-15 01:15:02 +0200

    Add FakeAgent for test purpose.

---
+++ test/fake.agent.ts
@@ -0,0 +1,14 @@
+import { AbstractAgent } from '../src/ai/agent/abstract.agent';
+import { AgentOption } from '../src/ai/options/agent.option';
+import { Inject } from '@nestjs/common';
+
+export class FakeAgent extends AbstractAgent {
+  constructor(@Inject option: AgentOption) {
+    super(
+      [], option);
+  }
+
+  async process(input: any): Promise<any> {
+    return input;
+  }
+}
